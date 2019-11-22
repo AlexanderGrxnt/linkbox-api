cee527e3611fb3294ae97f6d69761f861794eca8
AlexanderGrxnt <alexandergrxnt@gmail.com>
2019-11-22 21:07:49 +0000

    1st commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+        
+    }
+});
+
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    res.send('It is working!');
+})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
+})
+
