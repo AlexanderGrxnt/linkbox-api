cf989bd49fce682d38472e24e8a8c6019b9e392a
AlexanderGrxnt <alexandergrxnt@gmail.com>
2019-12-10 22:34:22 +0000

    mobile view

---
+++ public/javascript/live_script.js
@@ -0,0 +1,34 @@
+const domain = String(window.location.origin);
+let media = document.getElementsByClassName("media");
+let linkArr;
+let logoArr;
+let profileURL;
+
+function fetchData(){
+    fetch(`${domain}/data`)
+    .then(response => {
+      return response.json();
+    }).then(data => {
+      // Work with JSON data here
+      profileURL = data.profileImg;
+      linkArr = data.linkArr;
+      logoArr = data.logoArr;
+      console.log(logoArr);
+      spreadURLs();
+    }).catch(err => {
+      // Do something for an error here
+      console.log("error");
+    });
+    
+  }
+  
+  fetchData();
+  
+  function spreadURLs(){
+      let mediaArr = [...media];
+      for(let i = 0; i < logoArr.length; i++){
+        mediaArr[i].src = `${domain}/logos/${logoArr[i]}_logo.png`;
+      }
+      console.log(mediaArr[3].src);
+  }
+  
--- server.js
+++ server.js
@@ -12,26 +12,26 @@ const save = require('./controllers/save');
 const signout = require('./controllers/signout');
 
 const db = knex({
-    client: 'pg',
-    connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true,        
-    }
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  }
 });
 
 const app = express();
 
 app.use(session({
-	secret: 'linkbox',
-	resave: true,
-	saveUninitialized: true
+  secret: 'linkbox',
+  resave: true,
+  saveUninitialized: true
 }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.set('views', __dirname + '/views');
-app.use( express.static( "public" ) );
+app.use(express.static("public"));
 app.engine('ejs', require('ejs').__express);
 app.set('view engine', 'ejs');
 
@@ -39,67 +39,88 @@ var sesh;
 
 //SERVER START
 app.get('/', (req, res) => {
-    sesh = req.session; 
-    if(sesh.email) {
-      let email = sesh.email;
-      res.redirect('/profile');
-      //res.render(__dirname + "views/profile.ejs", {email:"email test"});
-    } else {
-      res.sendFile('index.html', {root : __dirname + '/views'});
-    }
+  sesh = req.session;
+  if (sesh.email) {
+    let email = sesh.email;
+    res.redirect('/profile');
+    //res.render(__dirname + "views/profile.ejs", {email:"email test"});
+  } else {
+    res.sendFile('index.html', { root: __dirname + '/views' });
+  }
 })
 
-app.get('/profile',function(req,res){
-    sesh = req.session;
-    if(sesh.email) {
-      var email = sesh.email;
-      var username = sesh.username;
-      var profile_img = sesh.profile_img;
-      if(profile_img === null)
-        profile_img = "/logos/plus_image.png";
+app.get('/profile', function (req, res) {
+  sesh = req.session;
+  if (sesh.email) {
+    var email = sesh.email;
+    var username = sesh.username;
+    var profile_img = sesh.profile_img;
+    if (profile_img === null)
+      profile_img = "/logos/plus_image.png";
 
-      res.render("profile.ejs", {
-        email:email,
-        username: username,
-        profile_img: profile_img
-      });
-    } else {
-      res.write('<h1>login first.</h1>');
-      res.end('<a href="/">Login</a>');
-    }
-  });
+    res.render("profile.ejs", {
+      email: email,
+      username: username,
+      profile_img: profile_img
+    });
+  } else {
+    res.write('<h1>login first.</h1>');
+    res.end('<a href="/">Login</a>');
+  }
+});
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt, sesh) })
 
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt, sesh) })
 
-app.get('/signout', (req, res, next) => { signout.handleSignout(req, res, db, sesh) 
+app.get('/signout', (req, res, next) => {
+  signout.handleSignout(req, res, db, sesh)
 })
 
+//START OF ONLINE CODE ONLY
+
 app.put('/save', (req, res) => { save.handleSave(req, res, db, sesh) })
-//app.put('/save', (req, res) => { res.end('ended') })
 
 app.get('/data', (req, res) => {
   let logoArray = req.session.logoArr;
-  if(logoArray === null){
-      logoArray = ['facebook', 'Instagram', 'youtube', 'twitter', 'snapchat', 'gmail'];
+  if (logoArray === null) {
+    logoArray = ['facebook', 'Instagram', 'youtube', 'twitter', 'snapchat', 'gmail'];
   }
   res.json({
-    
+
     profileImg: req.session.profile_img,
     linkArr: req.session.linkArr,
     logoArr: logoArray
   });
 })
 
+app.get('/:username', (req, res) => {
+  const userID = req.params.username;
+
+  db.select('profile_img').from('users')
+    .where('username', '=', userID)
+    .then(data => {
+      let profile_img = data[0].profile_img;
+      if (profile_img === null)
+        profile_img = "/logos/profile.png";
+      res.render("alexander.ejs", {
+        profile_img: profile_img,
+        username: userID,
+      });
+    })
+})
+
+//END OF ONLINE CODE
 
 //OFFLINE TEST
-// app.get('/signin',(req,res) => {
+//app.put('/save', (req, res) => { res.end('ended') })
+
+//app.get('/signin',(req,res) => {
 //   sesh = req.session;
 //   var email = "alex@gmail.com";
 //   var username = "AlexanderGrxnt";
 //   var profile_img = "https://picsum.photos/200";
-  
+
 //   res.render("profile.ejs", {
 //     email: email,
 //     username: username,
@@ -116,32 +137,20 @@ app.get('/data', (req, res) => {
 //   });
 // })
 
+// app.get('/:username', (req, res) => {
+//   const userID = req.params.username;
+//   res.render("live_page.ejs", {
+//     profile_img: "/logos/profile.png",
+//     username: userID,
+//   });
+// })
+
 //END OF OFFLINE TEST
 
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT}`);
+  console.log(`app is running on port ${process.env.PORT}`);
 })
 
-app.get('/:username', (req, res) => {
-    const userID = req.params.username;
 
-    db.select('profile_img').from('users')
-   .where('username', '=', userID)
-   .then(data => {
-    //console.log(data[0].profile_img);
-      res.render("alexander.ejs", {
-        profile_img: data[0].profile_img,
-        username: userID,
-        //profile_img: data[0].profile_img
-      });
-
-   })
-    
-    // var profile_img = sesh.profile_img;
-    // if(profile_img === null)
-    //   profile_img = "/logos/profile.png";
-    
-    
-})
 
